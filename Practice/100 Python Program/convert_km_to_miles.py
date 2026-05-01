e06d741a31b40890c98807fe0a2f5caed7a6120c
Sourav Sharma <souravsid66@gmail.com>
2026-05-01 09:41:39 +0530

    Cleanup: Keeping only Gym Tracker files

--- Practice/100 Python Program/convert_km_to_miles.py
+++
@@ -1,4 +0,0 @@
-"""Convert Kilometers to Miles"""
-km = float(input("enter the number:"))
-miles = km*(0.621371)
-print(km, "km in miles will be:", miles,"miles")
--- mymath/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Yeh mymath package ki init file hai jisme basic aur stats modules hain.
-"""
-from . import basic
-from . import stats
